83afedcf75ceb4d395be9484c56831fa4623c0a6
adama seye <seyeadam1@gmail.com>
2023-12-02 20:38:03 +0100

    add vault config

--- src/main/java/com/sid/gl/Auth2factorApplication.java
+++ src/main/java/com/sid/gl/Auth2factorApplication.java
@@ -1,12 +1,18 @@
 package com.sid.gl;
 
+import com.sid.gl.config.ApiKeyCredential;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class Auth2factorApplication {
+@EnableConfigurationProperties(ApiKeyCredential.class)
+public class Auth2factorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Auth2factorApplication.class, args);
@@ -15,6 +21,23 @@ public class Auth2factorApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	private Logger logger= LoggerFactory.getLogger(Auth2factorApplication.class);
+
+	private ApiKeyCredential credential;
+
+	public Auth2factorApplication(ApiKeyCredential credential) {
+		this.credential = credential;
+	}
+
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("------------Starting app ---------");
+		//logger.info("apikey email : "+credential.getApikey());
+
 	}
 
 
---
+++ src/main/java/com/sid/gl/config/ApiKeyCredential.java
@@ -0,0 +1,10 @@
+package com.sid.gl.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties("adama")
+public class ApiKeyCredential {
+    private String apikey;
+}
